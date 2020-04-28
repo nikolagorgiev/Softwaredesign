dc6c18e9111f9b070bb314ef7a32153d4bf3bf9c
nikolagorgiev <48891699+nikolagorgiev@users.noreply.github.com>
2020-04-28 11:37:18 +0200

    Added namespace / updates

--- Hello_World/main.js
+++ Hello_World/main.js
@@ -1,2 +1,6 @@
-console.log("Hello World");
+"use strict";
+var L02_Helloworld;
+(function (L02_Helloworld) {
+    console.log("Hello World");
+})(L02_Helloworld || (L02_Helloworld = {}));
 //# sourceMappingURL=main.js.map
--- Hello_World/main.ts
+++ Hello_World/main.ts
@@ -1,1 +1,3 @@
-console.log("Hello World");
+namespace L02_Helloworld {
+    console.log("Hello World");
+}
